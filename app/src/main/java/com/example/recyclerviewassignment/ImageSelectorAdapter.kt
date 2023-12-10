2f280e6143d4be2c8b1717042c8bb272ae538711
Kajal-Pandhare <kajal.pandhare14@gmail.com>
2023-12-10 13:33:23 +0530

    RecyclerView Assignment

--- app/src/main/java/com/example/recyclerviewassignment/ImageSelectorAdapter.kt
+++ app/src/main/java/com/example/recyclerviewassignment/ImageSelectorAdapter.kt
@@ -11,7 +11,7 @@ import android.widget.Toast
 import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.recyclerview.widget.RecyclerView
 
-class ImageSelectorAdapter(private val imageList : ArrayList<ImageSelect>):
+class ImageSelectorAdapter(private var imageList : ArrayList<ImageSelect>):
 RecyclerView.Adapter<ImageSelectorAdapter.ImageViewHolder>(){
 
     interface OnImageClickListener{
@@ -20,13 +20,25 @@ RecyclerView.Adapter<ImageSelectorAdapter.ImageViewHolder>(){
     var onImageClickListener : OnImageClickListener? = null
 
     inner class ImageViewHolder(view: View): RecyclerView.ViewHolder(view){
-       val image : ImageView
-       val edtUserName : TextView
-       val edtAddress : TextView
+        val selectedImage:ImageView
+       val userName : TextView
+       val Address : TextView
+
         init {
-            image = view.findViewById(R.id.img1)
-            edtUserName = view.findViewById(R.id.edtUserName)
-            edtAddress = view.findViewById(R.id.edtAddress)
+            selectedImage = view.findViewById(R.id.imageView)
+            userName = view.findViewById(R.id.txtUsername)
+            Address = view.findViewById(R.id.txtAddress)
+
+            itemView.setOnClickListener{
+                if (onImageClickListener!=null){
+                    onImageClickListener!!.onImageListener(
+                        adapterPosition,
+                        imageList[adapterPosition],
+                        it
+                    )
+                }
+
+            }
         }
     }
 
@@ -34,16 +46,16 @@ RecyclerView.Adapter<ImageSelectorAdapter.ImageViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.activity_item_select,null)
+        val view = layoutInflater.inflate(R.layout.view_image,null)
 
         return ImageViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
 
-        holder.image.setImageResource(imageList[position].image)
-        holder.edtUserName.text = imageList[position].userName
-        holder.edtAddress.text = imageList[position].address
+        holder.selectedImage.setImageResource(imageList[position].image)
+        holder.userName.text = imageList[position].userName
+        holder.Address.text = imageList[position].address
 
     }
 }
--- app/src/main/java/com/example/recyclerviewassignment/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewassignment/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.recyclerviewassignment
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
@@ -7,20 +8,25 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
 
-    private lateinit var menuPlus : Menu
+    private lateinit var menuPlus: Menu
     private lateinit var imageSelectorAdapter: ImageSelectorAdapter
     private lateinit var itemSelect: ItemSelect
 
     val imageList = ArrayList<ImageSelect>()
+    private lateinit var username: String
+    private lateinit var address: String
+    private var selectedImage: Int = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,22 +39,96 @@ class MainActivity : AppCompatActivity() {
         imageSelectorAdapter = ImageSelectorAdapter(imageList)
         recyclerView.adapter = imageSelectorAdapter
 
-        recyclerView.layoutManager =
-            LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
 
+
+        recyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        initListener()
+    }
+
+    fun initListener() {
+        imageSelectorAdapter.onImageClickListener = MyOnItemClickListener()
+    }
+
+    private inner class MyOnItemClickListener : ImageSelectorAdapter.OnImageClickListener {
+
+        override fun onImageListener(position: Int, imageSelect: ImageSelect, view: View) {
+           // Toast.makeText(this@MainActivity, "clicked", Toast.LENGTH_SHORT).show()
+            // val intent = Intent(this@MainActivity,DetailActivity::class.java)
+            val intent = Intent(this@MainActivity, DetailActivity::class.java)
+            intent.putExtra("position", position)
+            intent.putExtra("userName", imageList[position].userName)
+            intent.putExtra("Address", imageList[position].address)
+            intent.putExtra("selectedImage", imageList[position].image)
+
+
+            startActivityForResult(intent, 2)
+            Toast.makeText(this@MainActivity, "listener", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_activity, menu)
         val itemMenu = menu?.findItem(R.id.itemMenu)
-        return super.onCreateOptionsMenu(menu)
-        }
+        //return super.onCreateOptionsMenu(menu)
+        return true
+    }
 
+    @SuppressLint("SuspiciousIndentation")
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
-        val Intent = Intent(this,ItemSelect::class.java)
-            startActivity(Intent)
+        val intent = Intent(this, ItemSelect::class.java)
+        startActivityForResult(intent, 1)
 
         return super.onOptionsItemSelected(item)
     }
+
+    @Deprecated("Deprecated in Java")
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 1 && resultCode == 1) {
+            username = data?.getStringExtra("userName").toString()
+            address = data?.getStringExtra("Address").toString()
+            selectedImage = data?.getIntExtra("selectedImage", 0)!!.toInt()
+
+            imageList.add(
+                ImageSelect(
+                    selectedImage,
+                    username,
+                    address
+                )
+            )
+            imageSelectorAdapter.notifyDataSetChanged()
+        }
+
+        if (requestCode == 2 && resultCode == 3) {
+
+            val position = data?.getIntExtra("position", -1)
+
+            if (position != -1) {
+                username = data?.getStringExtra("userName").toString()
+                address = data?.getStringExtra("Address").toString()
+                selectedImage = data?.getIntExtra("detailImage", 0).toString().toInt()
+            }
+                if (position != null) {
+                   imageList[position] = ImageSelect(selectedImage,username,address)
+                    imageSelectorAdapter.notifyItemChanged(position)
+                }
+        }
+        if (requestCode == 2 && resultCode == 4){
+            val position = data?.getIntExtra("position",-1)
+
+            if (position != -1){
+                if (position != null){
+                    imageList.removeAt(position)
+                }
+                if (position != null){
+                    imageSelectorAdapter.notifyItemRemoved(position)
+                }
+            }
+        }
     }
+
+}
